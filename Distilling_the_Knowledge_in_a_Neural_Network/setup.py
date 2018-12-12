2ca7c2cf0ee730816c35361e88a796a16ba021d4
satishjasthi <satish27may@gmail.com>
2018-12-12 15:07:13 +0530

    Create Distilling_the_Knowledge_in_a_Neural_Network using CookieCutter

---
+++ Distilling_the_Knowledge_in_a_Neural_Network/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Implementation of deep learning papers related to Transfer learning of DL models, Generalization and understanding of DL models.',
+    author='Satish_Jasthi',
+    license='MIT',
+)
